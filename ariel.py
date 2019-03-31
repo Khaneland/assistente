2f41f1968fea03c7326b6c8c1b1e2b8c6e536bf9
Khaneland <bruno.silva1643@etec.sp.gov.br>
2019-03-31 01:01:30 -0300

    Agora o assistente tem uma interface com input para poder fazer as perguntas, entre outras implementações que foram feitas

--- ariel.py
+++ ariel.py
@@ -1,16 +1,52 @@
+import wx
 import wikipedia
 import wolframalpha
 
-while True:
-    input = raw_input ("Q: ")
+class MyFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None,
+            pos=wx.DefaultPosition, size=wx.Size(450, 100),
+            style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
+             wx.CLOSE_BOX | wx.CLIP_CHILDREN,
+            title="Ariel")
+        panel = wx.Panel(self)
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        lbl = wx.StaticText(panel,
+        label="Hello I am Ariel your digital assistant. How can I help you?")
+        my_sizer.Add(lbl, 0, wx.ALL, 5)
+        self.txt = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER,size=(400,30))
+        self.txt.SetFocus()
+        self.txt.Bind(wx.EVT_TEXT_ENTER, self.OnEnter)
+        my_sizer.Add(self.txt, 0, wx.ALL, 5)
+        panel.SetSizer(my_sizer)
+        self.Show()
     
-    try:
-        #wolframalpha
-        app_id = "5395J6-5WTJ3AGLKT"
-        client = wolframalpha.Client(app_id)
-        res = client.query(input)
-        answer = next(res.result).text
-        print answer
-    except:
-        #wikipedia
-        print.wikipedia.summary(input)
+    def OnEnter(self, event):
+        input = self.txt.GetValue()
+        input = input.lower()
+        try:
+            #wolframalpha
+            app_id = "5395J6-5WTJ3AGLKT"
+            client = wolframalpha.Client(app_id)
+            res = client.query(input)
+            answer = next(res.result).text
+            print answer
+        except:
+            #wikipedia
+            input = input.split(' ')
+            input = " ".join(input[2:])
+            print wikipedia.summary(input)
+ 
+if __name__ == "__main__":
+    app = wx.App(True)
+    frame = MyFrame()
+    app.MainLoop()
+
+
+
+
+# while True:
+#     input = raw_input ("Q: ")
+    
+    
+        
